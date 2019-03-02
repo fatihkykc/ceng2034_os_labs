33c4b1d78ae3f3f74dd8739d75410299e15e3ad8
Fatih Kıyıkçı <32556019+fatihkykc@users.noreply.github.com>
2019-03-03 02:21:16 +0300

    Update answer0.py

--- assignment0/answer0.py
+++ assignment0/answer0.py
@@ -1,15 +1,15 @@
 import os, time
 
-os.chdir(os.path.expanduser("~")) ##change directory to home
-folder = "os_lab_0" ##define a folder variable
+os.chdir(os.path.expanduser("~")) 
+folder = "os_lab_0"
 if os.path.exists(folder) is False:
-	os.mkdir(folder) ##create a folder named "os_lab_0"
-os.chdir(folder) ##change the directory to the folder
+	os.mkdir(folder)
+os.chdir(folder) 
 
 myFiles= ['1.txt','2.txt','1.py']
 for file in myFiles:
 	if os.path.exists(file) is False:
-		os.mknod(file) ##create a .txt file
-	print("{} Date modified: {}".format(file,time.ctime(os.path.getmtime(file))))  ##reorganize the time
+		os.mknod(file)
+	print("{} Date modified: {}".format(file,time.ctime(os.path.getmtime(file)))) 
 	if file.endswith('.txt'):
 		print(file)
